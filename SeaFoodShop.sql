b2d0c81d9c60aa391a4749b9fe3d3c93a47e372a
AnhMinh0203 <Leanhminh0203@gmail.com>
2024-03-16 09:37:21 +0700

    Update db

--- SeaFoodShop.sql
+++ SeaFoodShop.sql
@@ -1,24 +1,21 @@
-create database SeaFoodShop
+﻿create database SeaFoodShop
 go
 use SeaFoodShop
 go
 create table Types (
 	Id int identity(1,1) primary key,
-	NameType nvarchar (20),
-	CreateDate date,
-	CreateBy uniqueidentifier,
-	ModifyDate date,
-	ModifyBy uniqueidentifier
+	NameType nvarchar (20)
 )
 go
-create table SeaFoods (
+
+create table SeaFoodDetail(
 	Id int identity(1,1) primary key,
-	Name nvarchar (50),
-	Description nvarchar(max),
-	Price decimal (10,2),
-	Unit nvarchar(10),
+	IdDescription int foreign key references Descriptions(Id),
 	Quantity int,
-	IdType int foreign key references Types(Id),
+	Instruct nvarchar (255) null,
+	ExpirationDate nvarchar (255) null,
+	Origin nvarchar (30) null,
+	PrimaryImage varchar(max) null,
 	CreateDate date,
 	CreateBy uniqueidentifier,
 	ModifyDate date,
@@ -26,8 +23,28 @@ create table SeaFoods (
 )
 go
 
+create table Descriptions (
+	Id int identity(1,1) primary key,
+	Description nvarchar(max)
+)
+go
+
+create table SeaFoods (
+	Id int identity(1,1) primary key,
+	IdSeaFoodDetail int foreign key references SeaFoodDetail(Id),
+	Name nvarchar (50),
+	Price decimal (10,2),
+	Unit nvarchar(10),
+	IdType int foreign key references Types(Id),
+	IdVoucher int foreign key references Vouchers(Id)
+)
+select * from SeaFoods
 create table Images (
-	IdFood int foreign key references SeaFoods(Id),
+	Id int identity(1,1) primary key,
+	IdFood int foreign key references SeaFoodDetail(Id),
+	IdComment int foreign key references Comments(Id),
+	IdBlog int foreign key references Blogs(Id),
+	IdDescription int foreign key references Descriptions(Id),
 	Image nvarchar(max)
 )
 go
@@ -38,10 +55,17 @@ create table Users (
 	PhoneNumber varchar (15),
 	Password varchar(max),
 	FullName nvarchar (30),
+	Gender int null,
 	Role int ,
-	Status int ,
+	Status int 
 )
-go;
+go
+create table FavoriteSeaFoods(
+	IdUser uniqueidentifier foreign key references Users(Id),
+	IdFood int foreign key references SeaFoods(Id)
+)
+go
+
 
 create table Vouchers(
 	Id int identity(1,1) primary key,
@@ -67,14 +91,15 @@ go
 create table Comments (
 	Id int identity(1,1) primary key,
 	Comment nvarchar(max),
-	Image nvarchar(max),
 	LikeCount int,
 	Dislike int,
-	Status int,
+	Status varchar(10),
+	Stars int,
 	IdFood int foreign key references SeaFoods(Id),
 	IdUser uniqueidentifier foreign key references Users(Id),
+	IdBlog int foreign key references Blogs(Id)
 )
-go
+go;
 
 create table ShoppingCart(
 	IdFood int foreign key references SeaFoods(Id),
@@ -85,12 +110,80 @@ go
 
 create table Address(
 	Id uniqueidentifier primary key,
-	NameAddress nvarchar(max)
+	NameAddress nvarchar(max),
+	PhoneNumber varchar(10),
+	IsDefault int 
 )
 go
 
+
 create table MapUserAndAddress(
 	IdUser uniqueidentifier foreign key references Users(Id),
-	IdAddress uniqueidentifier foreign key references Address(Id)
+	IdAddress uniqueidentifier foreign key references Address(Id),
+	Action int
 )
 go
+
+CREATE TABLE Blogs (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+	IdUser uniqueidentifier foreign key references Users(Id),
+    Title NVARCHAR(255) NOT NULL,
+    Content NVARCHAR(MAX) NOT NULL,
+    PublishedDate DATETIME NOT NULL,
+    Thumbnail NVARCHAR(MAX),
+    Views INT DEFAULT 0,
+    Likes INT DEFAULT 0
+);
+
+go
+
+
+INSERT INTO Blogs (IdUser, Title, Content, PublishedDate, Thumbnail, Views, Likes)
+VALUES 
+    ('ED2A419B-2B52-4039-9A80-BCC9C3BC6AAB', N'TẠI SAO ỐC BULOT IRELAND ĐƯỢC ƯA CHUỘNG TRÊN BÀN TIỆC ?', N'1. Nguồn gốc xuất xứ 
+
+Ốc bulot sinh sống ở các vùng biển trong lành tại khu vực Bắc Âu như Ireland, Pháp, Ireland, Bồ Đào Nha, Canada... Tuy nhiên, ốc bulot thường tập trung số lượng lớn và sinh trưởng tốt tại Ireland. 
+
+Mặc dù cùng là loài ốc bulot nhưng tại các vùng biển khác nhau, chúng sẽ có một số điểm khác biệt về vẻ ngoài cũng như hương vị đặc trưng.
+
+Ốc bulot Ireland có thịt dày, không lẫn cát đá, khi ăn cảm nhận được độ mềm mịn cùng hương vị tươi ngon. Chúng có vỏ màu kem nhạt được tô điểm bởi những vệt màu xanh lục, kích thước trung bình từ 6 - 10cm. Những con ốc bulot sinh sống ngoài tự nhiên có thể có tuổi thọ lên đến 10 năm.', '2024-03-15 10:00:00', 'path_to_thumbnail1.jpg', 100, 20);
+
+
+
+select * from blogs
+delete from blogs
+
+delete from Types
+delete from Images
+delete from seafooddetail
+delete from seafoods
+delete from ShoppingCart
+delete  from Comments
+delete from Descriptions
+-- Types
+insert into Types(NameType) values (N'Chả'),(N'Cá'),(N'Tôm'),(N'Mực')
+
+-- Description
+Insert into descriptions (Description) values (N'Chả mực Hạ Long là một đặc sản nổi tiếng của vùng biển Quảng Ninh, Việt Nam, nơi nổi tiếng với vị ngọt của mực và hương vị độc đáo của gia vị. Chả mực được làm từ những con mực tươi ngon, được tinh chế và chế biến theo quy trình truyền thống được lưu truyền từ đời này sang đời khác.')
+
+-- SeaFoodsDetail
+insert into SeaFoodDetail (Instruct, ExpirationDate, Origin, IdDescription)
+values (N'- Khi rán nên để nhỏ lửa, tránh để lửa to', 'Ngày 13/09/2024', N'Quảng Ninh',1)
+
+-- SeaFood 
+insert into SeaFoods (IdSeaFoodDetail,Name,Price,Unit,IdType,Status)
+values (1,N'Chả mực Hạ Long',500000,'Kg',1,0)
+
+
+select * from users
+select * from images
+select * from comments
+select * from ShoppingCart
+select * from seafoods
+select * from seafooddetail
+select * from Comments
+select * from ShoppingCart
+select * from Types
+select * from Descriptions
+
+DBCC CHECKIDENT ('seafooddetail', RESEED, 0);
--- StoreProcedure.sql
+++ StoreProcedure.sql
@@ -1,10 +1,10 @@
--- Sign In
+﻿-- Sign In
 CREATE PROCEDURE SignIn
     @phoneNumber VARCHAR(15)
 AS
 BEGIN
     DECLARE @sql NVARCHAR(MAX);
-    SET @sql = N'SELECT Password, Role from users where PhoneNumber = @phoneNumber';
+    SET @sql = N'SELECT Id, Password, Role from users where PhoneNumber = @phoneNumber';
     EXEC sp_executesql @sql, N'@phoneNumber VARCHAR(15)', @phoneNumber;
 END;
 
@@ -30,10 +30,456 @@ else
 	end;
 end;
 
+-- Display Seafoods
+alter PROCEDURE GetSeaFoods
+    @PageNumber INT,
+    @PageSize INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @Offset INT = (@PageNumber - 1) * @PageSize;
+
+    SELECT Id, Name, Price, Unit, Status
+    FROM SeaFoods
+    ORDER BY Id
+    OFFSET @Offset ROWS
+    FETCH NEXT @PageSize ROWS ONLY;
+END;
 
 
---DECLARE @result NVARCHAR(100);
---EXECUTE SignUp '2003-03-02', '0869819316', 'MinhLe123', N'L� Anh Minh', @result OUTPUT;
---print @result;
-execute SignIn '0869819316';
-select * from users;
+-- Dispay seafood detail
+alter procedure GetSeaFoodDetail
+@Id int
+as
+begin
+	SELECT 
+	s.Id as Id,
+	s.Name AS Name,
+    s.Price AS Price,
+    s.Unit AS Unit,
+	s.Status as Status,
+    t.NameType AS TypeName,
+    d.Description as Description, 
+    sf.Instruct as Instruct,
+	sf.ExpirationDate as ExpirationDate,
+	sf.Origin as Origin,
+    (SELECT Image FROM Images i WHERE i.IdDescription = d.Id FOR JSON PATH) AS DescriptionImages,
+    (SELECT Image FROM Images i WHERE i.IdFood = sf.Id for json path) AS SeaFoodImages
+	FROM 
+		descriptions d
+	INNER JOIN 
+		SeaFoodDetail sf ON d.Id = sf.IdDescription
+	INNER JOIN 
+        SeaFoods s ON sf.Id = s.IdSeaFoodDetail
+    INNER JOIN 
+        Types t ON s.IdType = t.Id
+	where d.Id = @Id
+end;
+GetSeaFoodDetail 1
+
+-- Search seafood by name
+create procedure SearchSeaFood
+@NameSeaFood nvarchar(50)
+as
+begin
+	select * from SeaFoods where Name like '%' + @NameSeaFood + '%'
+end;
+
+-- Search by type
+create procedure SearchSeaFoodByType
+@NameType nvarchar(50)
+as
+begin 
+	select SeaFoods.*, Types.NameType from SeaFoods Join Types on SeaFoods.IdType = Types.Id where Types.NameType = @NameType
+end;
+
+-- Push comment
+CREATE TYPE ImageTableType AS TABLE (
+    Image nvarchar(max)
+);
+
+alter procedure PostComment
+	@CommentText nvarchar(max),
+	@Images ImageTableType readonly,
+	@Like int ,
+	@Dislike int,
+	@IdFood int,
+	@IdUser uniqueidentifier,
+	@Stars int
+as
+begin 
+	INSERT INTO Comments (Comment, LikeCount, Dislike, Status, IdFood, IdUser, Stars)
+    VALUES (@CommentText, @Like, @Dislike, 'Post', @IdFood, @IdUser, @Stars);
+
+    -- Get the Id of the newly inserted comment
+    DECLARE @CommentId int;
+    SET @CommentId = SCOPE_IDENTITY();
+
+    -- Insert the image into the Images table with the corresponding comment Id
+    INSERT INTO Images (IdComment, Image)
+	SELECT @CommentId, Image
+	FROM @Images;
+end;
+
+-- Update comment
+alter PROCEDURE UpdateComment
+    @CommentId int,
+    @CommentText nvarchar(max),
+    @Images ImageTableType readonly,
+    @Stars int
+AS
+BEGIN
+    UPDATE Comments
+    SET Comment = @CommentText,
+        Stars = @Stars,
+		Status = 'Update'
+    WHERE Id = @CommentId;
+
+    -- Update all images for the comment
+	Delete from Images where IdComment = @CommentId
+	INSERT INTO Images (IdComment, Image)
+	SELECT @CommentId, Image
+	FROM @Images;
+END;
+
+
+-- Delete comment
+alter procedure DeleteComment
+	@Id int
+as
+begin
+	delete from images where IdComment = @Id
+	delete from Comments where Id = @Id
+end;
+
+-- Get comments
+create procedure GetComments
+	@PageNumber int,
+	@PageSize int
+as
+begin
+	set nocount on;
+	declare @Offset int = (@PageNumber - 1) * @PageSize;
+
+	SELECT
+        C.Id AS CommentId,
+        C.Comment,
+        C.LikeCount,
+        C.Dislike,
+        C.Status,
+        C.Stars,
+        I.Image
+    FROM
+        Comments C
+    INNER JOIN
+        Images I ON C.Id = I.IdComment
+    WHERE
+        C.Id = @CommentId;
+	order by Id
+	OFFSET @Offset rows
+	FETCH next @PageSize rows only;
+end;
+
+-- Search comments
+
+-- C1
+--alter procedure SearchComments
+--	@CommentText nvarchar(max)
+--as
+--begin
+--	SELECT Comment, Images.Image 
+--	FROM Comments 
+--	INNER JOIN Images ON Comments.Id = Images.IdComment
+--	WHERE Comment LIKE '%' + @CommentText + '%';
+--end;
+
+--C2
+alter procedure SearchComments
+	@CommentText nvarchar(max) 
+as
+begin
+	select c.*, (select i.image as nameImage from images i where i.IdComment = c.Id for Json Path) as Pictures
+	from Comments c 
+	where c.Comment like '%'+ @CommentText +'%'
+end
+
+-- Update seafood quantity in shopping cart
+alter procedure UpdateShoppingCart
+	@IdSeafood int,
+	@IdUser uniqueidentifier,
+	@Quantity int
+as
+begin
+	update ShoppingCart set Quantity = @Quantity where IdFood = @IdSeafood and IdUser = @IdUser;
+end;
+
+-- Add seafood to shopping cart
+alter PROCEDURE AddToShoppingCart
+    @IdSeafood INT,
+    @IdUser UNIQUEIDENTIFIER,
+    @Quantity INT
+AS
+BEGIN
+    MERGE INTO ShoppingCart AS Target
+    USING (VALUES (@IdSeafood, @IdUser, @Quantity)) AS Source (IdFood, IdUser, Quantity)
+    ON Target.IdFood = Source.IdFood AND Target.IdUser = Source.IdUser
+    WHEN MATCHED THEN
+        UPDATE SET Quantity = Target.Quantity + Source.Quantity
+    WHEN NOT MATCHED THEN
+        INSERT (IdFood, IdUser, Quantity) VALUES (Source.IdFood, Source.IdUser, Source.Quantity);
+END;
+
+-- Delete seafood in shopping cart
+create procedure DeleteShoppingCart 
+	@IdSeaFood int,
+	@IdUser uniqueidentifier
+as
+begin 
+	delete from ShoppingCart where IdFood = @IdSeaFood and IdUser = @IdUser;
+end;
+
+-- Search seafood in shopping cart
+alter procedure SearchShoppingCart
+	@IdUser uniqueidentifier,
+	@NameSeaFood nvarchar(50)
+as
+begin
+	if @NameSeaFood is not null
+		select sf.*  from SeaFoods sf inner join ShoppingCart sc on sf.Id = sc.IdFood where sc.IdUser = @IdUser and sf.Name like '%' + @NameSeaFood+ '%'
+	else
+		select sf.*  from SeaFoods sf inner join ShoppingCart sc on sf.Id = sc.IdFood where sc.IdUser = @IdUser
+end;
+
+-- Add type
+alter procedure AddSeaFoodType
+	@nameType nvarchar(20),
+	@result nvarchar(100) output
+as
+begin
+	if exists (select 1 from Types where NameType like '%' + @nameType + '%')
+	begin 
+		set @result =  N'Tên loại đã tồn tại'
+	end
+	else
+	begin
+		insert into Types(NameType) values (@nameType);
+		set @result =  N'Thêm loại mới thành công'
+	end
+end;
+
+-- Delete type
+alter procedure DeleteSeaFoodType
+	@nameType nvarchar(20),
+	@result nvarchar(100) output
+as
+begin
+	if not exists (select 1 from Types where NameType like '%' + @nameType + '%')
+	begin 
+		set @result =  N'Tên loại không tồn tại'
+	end
+	else
+	begin
+		delete from Types where NameType like '%' + @nameType + '%';
+		set @result =  N'Xóa loại thành công'
+	end
+end;
+
+-- Get all types
+alter procedure GetSeaFoodType
+as
+begin
+	select * from types 
+end;
+
+-- Update profile
+alter procedure UpdateProfile
+	@idUser uniqueidentifier,
+	@fullName nvarchar(30),
+	@dob date,
+	@gender int,
+	@phoneNumber nvarchar(15),
+	@result nvarchar(100) output
+as
+begin
+	update Users set FullName = @fullName,Dob = @dob, Gender = @gender, PhoneNumber = @phoneNumber where Id = @idUser
+	set @result = N'Cập nhật thông tin thành công'
+end;
+
+
+-- Get user profile
+alter procedure GetProfile
+	@idUser uniqueidentifier
+as
+begin
+	select Id,FullName, Dob, PhoneNumber, Gender from users where Id = @idUser
+end
+
+-- Change password
+create procedure GetPassword
+	@idUser uniqueidentifier
+as
+begin
+	select password from Users where Id = @idUser
+end
+
+alter procedure ChangePassword
+	@idUser uniqueidentifier,
+	@newPassword varchar(max)
+as
+begin
+	update Users set Password = @newPassword where Id = @idUser
+end;
+
+-- Get favorite seafoods
+alter procedure GetFavoriteSeafoods
+	@idUser uniqueidentifier
+as
+begin
+	select sf.Id, sf.Name, sf.Price, sf.Unit,sf.Status, sf.IdVoucher from SeaFoods sf 
+	inner join FavoriteSeaFoods fs on sf.Id = fs.IdFood 
+	where fs.IdUser = @idUser
+end
+
+-- Add favorite seafoods
+alter PROCEDURE AddFavoriteSeafood
+    @IdSeafood INT,
+    @IdUser UNIQUEIDENTIFIER
+AS
+BEGIN
+    INSERT into FavoriteSeafoods(IdUser, IdFood) VALUES (@IdUser,@IdSeafood);
+END;
+
+-- Delete favorite seafoods
+create procedure DeleteFavoriteSeafood
+	@IdSeaFood int,
+	@IdUser uniqueidentifier
+as
+begin 
+	delete from FavoriteSeafoods where IdFood = @IdSeaFood and IdUser = @IdUser;
+end;
+
+--Add Address
+alter procedure AddAddress
+	@idUser uniqueidentifier,
+	@idAddress uniqueidentifier,
+	@nameAddress nvarchar(max),
+	@phoneNumber varchar(10),
+	@isDefault int,
+	@result nvarchar(100) output
+as 
+begin
+	INSERT INTO Address (Id, NameAddress, PhoneNumber, IsDefault) VALUES (@idAddress, @nameAddress, @phoneNumber, @isDefault);
+	INSERT INTO MapUserAndAddress (IdAddress, IdUser,Action) VALUES (@idAddress, @idUser,1);
+	set @result = N'Thêm mới địa chỉ thành công'
+end
+
+-- Get Address
+alter procedure GetAddresses
+	@idUser uniqueidentifier
+as
+begin
+	select ad.* from Address ad
+	inner join MapUserAndAddress mu on mu.IdAddress = ad.Id
+	where mu.IdUser = @idUser and mu.Action = 1
+end;
+
+-- Delete Address
+create procedure DeteleAddress
+	@idUser uniqueidentifier,
+	@idAddress uniqueidentifier
+as
+begin
+	update MapUserAndAddress set Action = 0 where IdUser = @idUser and IdAddress = @idAddress
+end
+
+-- Read blog
+create procedure getBlogs
+as
+begin
+	select * from Blogs
+end
+
+
+
+
+
+-- draft
+select * from images
+
+GetProfile  'ED2A419B-2B52-4039-9A80-BCC9C3BC6AAB'
+UpdateProfile 'ED2A419B-2B52-4039-9A80-BCC9C3BC6AAB', 'Lê Anh Minhh', '2024-02-27',0,'0869819316'
+
+
+
+EXEC sp_rename 'DeleteTypeSeaFood', 'DeleteSeaFoodType'
+	SELECT Comment, Images.Image 
+	FROM Comments 
+	INNER JOIN Images ON Comments.Id = Images.IdComment
+	WHERE Comment LIKE '%' + 'C' + '%';
+
+	select * from Address
+	select * from MapUserAndAddress
+select * from Types
+select * from users
+select * from comments
+select * from Images
+select * from SeaFoods
+select * from SeaFoodDetail
+select * from descriptions
+select * from images
+select * from ShoppingCart
+
+
+
+insert into descriptions (Description) values('des')
+insert into images(IdDescription,Image) values(1,'Img Description 2')
+
+update SeaFoodDetail set IdDescription = 1 where Id = 1
+select * from SeaFoodDetail
+
+
+
+
+-- Thực thi thủ tục PushComment
+EXEC PushComment N'Nội dung bình luận', @Images, 10, 5, 1, 1, 'ED2A419B-2B52-4039-9A80-BCC9C3BC6AAB', 4;
+AddTypeSeaFood 'Type5'
+
+select * from users
+select * from images
+select * from comments
+select * from ShoppingCart
+DECLARE @Images ImageTableType;
+INSERT INTO @Images (Image) VALUES ('Image1New.jpg');
+
+EXEC UpdateComment
+    @CommentId = 10,
+    @CommentText = 'ahihidsfdfdfgdfhihi',
+    @Images = @Images,
+    @Stars = 4;
+
+SearchShoppingCart 'ED2A419B-2B52-4039-9A80-BCC9C3BC6AAB', '1'
+;with abc as 
+(
+	select 
+		c.*, 
+		(select i.Image from Images i where i.IdComment = c.Id for Json Path ) as Anh
+	from Comments c where  c.Comment  like '%C%'
+)
+
+select * from abc
+
+
+select * from Comments
+
+select c.* , (select i.Image as nameImage from Images i where i.IdComment = c.Id for Json Path ) as Pictures from Comments c where c.Comment like '%a%'
+select * from ShoppingCart
+
+merge product_list as target
+using update_list as source 
+on target.Id = source.id 
+when matched
+then update target table set target.Name = source.name , target.Price = source.price
+when not matched by target 
+then insert into target (target.Name,target.Price) values (source.name,source.price)
+when not matched by source 
+then delete 
